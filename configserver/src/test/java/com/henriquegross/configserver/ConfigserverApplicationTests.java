96a29c4aae9fb9b4dea81d2c0138aa456aa761fe
Henrique Grosscklauss <henriquegrossdeveloper@gmail.com>
2022-08-19 17:54:05 -0300

    Creating ConfigServer project and load all the configurations from github

---
+++ configserver/src/test/java/com/henriquegross/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henriquegross.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
