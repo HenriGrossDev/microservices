bfad1cab1462184ec318c54410b2f0c76cccca5d
Henrique Grosscklauss <henriquegrossdeveloper@gmail.com>
2022-08-22 11:01:13 -0300

    Updating Accounts, Loans and Cards microservices to read properties from config server

---
+++ cards/src/main/java/com/henriquegross/cards/controller/CardsController.java
@@ -0,0 +1,49 @@
+package com.henriquegross.cards.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.henriquegross.cards.CardsRepository;
+import com.henriquegross.cards.config.CardsServiceConfig;
+import com.henriquegross.cards.model.Cards;
+import com.henriquegross.cards.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import com.henriquegross.cards.model.Properties;
+
+@RestController
+public class CardsController {
+
+
+    @Autowired
+    private CardsRepository cardsRepository;
+
+    @Autowired
+    CardsServiceConfig cardsConfig;
+
+    @PostMapping("/myCards")
+    public List<Cards> getCardDetails(@RequestBody Customer customer) {
+        List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
+        if (cards != null) {
+            return cards;
+        } else {
+            return null;
+        }
+
+    }
+
+    @GetMapping("/cards/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(cardsConfig.getMsg(), cardsConfig.getBuildVersion(),
+                cardsConfig.getMailDetails(), cardsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
+    }
+
+}
--- loans/src/main/java/com/henriquegross/loans/controller/LoansController.java
+++ loans/src/main/java/com/henriquegross/loans/controller/LoansController.java
@@ -1,7 +1,12 @@
 package com.henriquegross.loans.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.henriquegross.loans.config.LoansConfigServer;
 import com.henriquegross.loans.model.Customer;
 import com.henriquegross.loans.model.Loans;
+import com.henriquegross.loans.model.Properties;
 import com.henriquegross.loans.repository.LoansRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +21,9 @@ public class LoansController {
     @Autowired
     private LoansRepository loansRepository;
 
+    @Autowired
+    LoansConfigServer loansConfigServer;
+
     @GetMapping("/myLoans")
     public List<Loans> getLoansDetails(@RequestBody Customer customer) {
         List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
@@ -24,6 +32,15 @@ public class LoansController {
         } else {
             return null;
         }
+    }
 
+    @GetMapping("/loans/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(loansConfigServer.getMsg(),
+                                               loansConfigServer.getBuildVersion(),
+                                               loansConfigServer.getMailDetails(),
+                                               loansConfigServer.getActiveBranches());
+        return ow.writeValueAsString(properties);
     }
 }
